fa2c8b22557ed0cab98dc265acd7e34c3912ddd9
CryptoCatOkiOki <34884057+CryptoCatOkiOKi@users.noreply.github.com>
2018-09-26 16:25:05 +0200

    find fork
    param1 => wallet alias on right chain
    param2 => starting block
    param3 => block increment

---
+++ find_fork.sh
@@ -0,0 +1,57 @@
+#/bin/bash
+
+WALLET=$1
+LASTBLOCK=$2
+INCREMENT=$3
+
+for (( ; ; ))
+do
+
+	for (( ; ; ))
+	do  
+		echo "LASTBLOCK=$LASTBLOCK"
+		
+		for filename in /root/bin/monkey-cli*.sh; do
+			echo $filename
+			GETBLOCKHASHWALLET=$(~/bin/monkey-cli_$WALLET.sh getblockhash $LASTBLOCK)
+			GETBLOCKHASH=$($filename getblockhash $LASTBLOCK)
+			
+			if [ -z "$GETBLOCKHASHWALLET" ]; then
+			  break
+			fi	
+
+			if [ -z "$GETBLOCKHASH" ]; then
+			  break
+			fi				
+			
+			echo "GETBLOCKHASHWALLET=$GETBLOCKHASHWALLET"
+			echo "GETBLOCKHASH=$GETBLOCKHASH"
+		done
+		
+		if [ -z "$GETBLOCKHASHWALLET" ]; then
+		 break
+		fi	
+		
+		if [ -z "$GETBLOCKHASH" ]; then
+		  break
+		fi			
+		
+		if [ "$GETBLOCKHASH" != "$GETBLOCKHASHWALLET" ]; then
+		  echo "FORK ON $LASTBLOCK !!!!"
+		  break
+		fi
+		
+		LASTBLOCK=$[LASTBLOCK + $INCREMENT]
+	done
+
+	LASTBLOCK=$[LASTBLOCK - $INCREMENT]
+	INCREMENT=$[INCREMENT / 10]
+	
+    if [[ "$INCREMENT" -eq 0 ]]; then
+	  break
+	fi	
+	
+    if [[ "$INCREMENT" -lt 1 ]]; then
+	  break
+	fi		
+done
